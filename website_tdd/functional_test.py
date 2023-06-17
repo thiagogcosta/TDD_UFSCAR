84d6f474c666353be235457fb5fb8ada7e6423d5
Thiago Costa <thiagogcosta@github.com>
2023-06-17 11:38:16 -0300

    update with dados on bd

--- website_tdd/functional_test.py
+++ website_tdd/functional_test.py
@@ -3,13 +3,19 @@ from selenium.webdriver.common.keys import Keys
 import time
 import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVsitorTest(unittest.TestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Chrome()
 
 	def tearDown(self):
 		self.browser.quit()
+
+	# Auxiliary method 
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# Edith ouviu falar de uma nova aplicação online interessante
@@ -38,34 +44,33 @@ class NewVisitorTest(unittest.TestCase):
 
 		inputbox.send_keys('Buy peacock feathers')
 
-
 		# Quando ela tecla enter, a página é atualizada, e agora
 		# a página lista "1 - Buy peacock feathers" como um item em 
 		# uma lista de tarefas
 
 		inputbox.send_keys(Keys.ENTER)
 		time.sleep(1)
-
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(
-			any(row.text == '1: Buy peacock feathers' for row in rows),
-			"New to-do item not appear in table"
-		)
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 		# Ainda continua havendo uma caixa de texto convidando-a a 
 		# acrescentar outro item. Ela insere "Use peacock feathers 
-		# make a fly" (Usar penas de pavão para fazer um fly - 
+		# to make a fly" (Usar penas de pavão para fazer um fly - 
 		# Edith é bem metódica)
-
-		self.fail('Finish the test!')
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys("Use peacock feathers to make a fly")
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
 
 		# A página é atualizada novamente e agora mostra os dois
 		# itens em sua lista
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
 		# Edith se pergunta se o site lembrará de sua lista. Então
 		# ela nota que o site gerou um URL único para ela -- há um 
 		# pequeno texto explicativo para isso.
+
+		self.fail('Finish the test!')
 
 		# Ela acessa essa URL -- sua lista de tarefas continua lá.
 
