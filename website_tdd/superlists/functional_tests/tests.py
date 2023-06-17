5fd33a50224ca0a0f4b831baa4faf922716bdaa0
Thiago Costa <thiagogcosta@github.com>
2023-06-17 14:43:19 -0300

    update

--- website_tdd/superlists/functional_tests/tests.py
+++ website_tdd/superlists/functional_tests/tests.py
@@ -1,21 +1,33 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 import time
 
-class NewVsitorTest(LiveServerTestCase):
+MAX_WAIT = 10
+
+class NewVisitorTest(LiveServerTestCase):
 
 	def setUp(self):
-		self.browser = webdriver.Firefox()
+		self.browser = webdriver.Chrome()
 
 	def tearDown(self):
 		self.browser.quit()
 
 	# Auxiliary method 
-	def check_for_row_in_list_table(self, row_text):
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn(row_text, [row.text for row in rows])
+	def wait_for_row_in_list_table(self, row_text):
+		start_time = time.time()
+		while True:
+			try:
+				table = self.browser.find_element_by_id('id_list_table')
+				rows = table.find_elements_by_tag_name('tr')
+				self.assertIn(row_text, [row.text for row in rows])
+				return
+			except(AssertionError, WebDriverException) as e:
+				if time.time() - start_time > MAX_WAIT:
+					raise e
+				time.sleep(0.5)
+
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# Edith ouviu falar de uma nova aplicação online interessante
@@ -50,7 +62,7 @@ class NewVsitorTest(LiveServerTestCase):
 
 		inputbox.send_keys(Keys.ENTER)
 		time.sleep(1)
-		self.check_for_row_in_list_table('1: Buy peacock featers')
+		self.wait_for_row_in_list_table('1: Buy peacock featers')
 
 		# Ainda continua havendo uma caixa de texto convidando-a a 
 		# acrescentar outro item. Ela insere "Use peacock feathers 
@@ -63,8 +75,8 @@ class NewVsitorTest(LiveServerTestCase):
 
 		# A página é atualizada novamente e agora mostra os dois
 		# itens em sua lista
-		self.check_for_row_in_list_table('1: Buy peacock featers')
-		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+		self.wait_for_row_in_list_table('1: Buy peacock featers')
+		self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
 		# Edith se pergunta se o site lembrará de sua lista. Então
 		# ela nota que o site gerou um URL único para ela -- há um 
